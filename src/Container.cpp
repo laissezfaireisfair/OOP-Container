bc35a47ff3e5a1b593e4e657b150993037b3dbd0
Andrey Golubev <laissezfaireisfair@protonmail.com>
2019-12-13 22:01:18 +0700

    Shoren types locally in Container source

--- src/Container.cpp
+++ src/Container.cpp
@@ -1,10 +1,15 @@
 #include "../include/Container.hpp"
 
 namespace Bicycle{
+  template <class T> using sPtr<T> = std::shared_ptr<T>;
+  template <class T> using wPtr<T> = std::weak_ptr<T>;
+  template <class T> using uPtr<T> = std::unique_ptr<T>;
+  using usInt = std::uint64_t;
+
   template <class T> T Container::get_back() const;
   template <class T> T Container::get_front() const;
-  template <class T> T Container::operator[](std::uint64_t const i);
-  template <class T> std::uint64_t Container::get_length() const;
+  template <class T> T Container::operator[](usInt const i);
+  template <class T> usInt Container::get_length() const;
   template <class T> bool Container::is_empty() const;
   template <class T> void Container::push_back(T const elem);
   template <class T> void Container::push_front(T const elem);
