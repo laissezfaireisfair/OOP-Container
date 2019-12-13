23b3f81b3114e90dbdcf48b22a2947d17c794326
Andrey Golubev <laissezfaireisfair@protonmail.com>
2019-12-13 22:48:04 +0700

    Add getters realisation

--- src/Container.cpp
+++ src/Container.cpp
@@ -1,4 +1,5 @@
 #include "../include/Container.hpp"
+#include <stdexcept>
 
 namespace Bicycle{
   template <class T> using sPtr<T> = std::shared_ptr<T>;
@@ -22,11 +23,27 @@ namespace Bicycle{
     for (usInt i = 0; i < length; ++i)
       body[i] = value;
   }
-  template <class T> T Container::get_back() const;
-  template <class T> T Container::get_front() const;
-  template <class T> T Container::operator[](usInt const i);
-  template <class T> usInt Container::get_length() const;
-  template <class T> bool Container::is_empty() const;
+  template <class T> T Container::get_back() const {
+    if (length == 0)
+      throw std::length_error("Zero length container dont have back elem");
+    return body[length - 1];
+  }
+  template <class T> T Container::get_front() const {
+    if (length == 0)
+      throw std::length_error("Zero length container dont have front elem");
+    return body[0];
+  }
+  template <class T> T Container::operator[](usInt const i) {
+    if (i >= length)
+      throw std::out_of_range("Container size is less than adress");
+    return body[0];
+  }
+  template <class T> usInt Container::get_length() const {
+    return length;
+  }
+  template <class T> bool Container::is_empty() const {
+    return length == 0;
+  }
   template <class T> void Container::push_back(T const elem);
   template <class T> void Container::push_front(T const elem);
   template <class T> void Container::pop_back(T const elem);
