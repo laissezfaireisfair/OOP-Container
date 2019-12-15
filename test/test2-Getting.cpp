cde190d63f8d434ebb3d8629af80557c8c7ad022
Andrey Golubev <laissezfaireisfair@protonmail.com>
2019-12-15 21:29:15 +0700

    Add getting values test

---
+++ test/test2-Getting.cpp
@@ -0,0 +1,25 @@
+#include "../include/Container.hpp"
+#include "../src/Container.cpp"
+#include <cstdint>
+#include <iostream>
+using namespace std;
+using namespace Bicycle;
+
+void print_int_container(Container<int> const & vec) {
+  const uint64_t length = vec.get_length();
+  for (uint64_t i = 0; i < length; ++i)
+    cout << vec[i] << " ";
+}
+
+int main() {
+  cout << "Making vec of five (666)s..." << endl;
+  Container<int> vec(666, 5);
+  cout << "Length: " << vec.get_length() << endl;
+  cout << "Is empty: " << (vec.is_empty() ? "True" : "False") << endl;
+  cout << "Front value: " << vec.get_front() << endl;
+  cout << "Back value: " << vec.get_back() << endl;
+  cout << "Printing vec..." << endl;
+  print_int_container(vec);
+  cout << endl << "Container printed." << endl;
+  return 0;
+}
