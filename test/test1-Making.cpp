d980b6ed16d487f6700f3b6d2226c61b37a3823f
Andrey Golubev <laissezfaireisfair@protonmail.com>
2019-12-15 21:12:45 +0700

    Add making container test

---
+++ test/test1-Making.cpp
@@ -0,0 +1,16 @@
+#include "../include/Container.hpp"
+#include "../src/Container.cpp"
+#include <iostream>
+using namespace std;
+using namespace Bicycle;
+
+int main() {
+  cout << "Making vec with default constructor..." << endl;
+  Container<int> vecDefault;
+  cout << "Making vec with copy constructor..." << endl;
+  Container<int> vecCopy = vecDefault;
+  cout << "Making vec with value constructor..." << endl;
+  Container<int> vecValues(666, 5);
+  cout << "All vectors was made." << endl;
+  return 0;
+}
