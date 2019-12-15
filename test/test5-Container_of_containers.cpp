ac7b45f664ed79d26b810466730d2e0be9438d32
Andrey Golubev <laissezfaireisfair@protonmail.com>
2019-12-15 22:12:56 +0700

    Add test for container of containers

---
+++ test/test5-Container_of_containers.cpp
@@ -0,0 +1,35 @@
+#include "../include/Container.hpp"
+#include "../src/Container.cpp"
+#include <cstdint>
+#include <iostream>
+using namespace std;
+using namespace Bicycle;
+
+void print_int_container(Container<int> const & vec) {
+  const uint64_t length = vec.get_length();
+  for (uint64_t i = 0; i < length; ++i)
+    cout << vec[i] << " ";
+}
+
+void print_matrix(Container<Container<int> > const & matrix) {
+  const uint64_t sizeStr = matrix.get_length();
+  for (uint64_t i = 0; i < sizeStr; ++i) {
+    print_int_container(matrix[i]);
+    cout << endl;
+  }
+}
+
+int main() {
+  cout << "Making vec of five int Containers..." << endl;
+  Container<Container<int> > matrix(Container<int>(), 5);
+  cout << "Setting values..." << endl;
+  for (unsigned int i = 0; i < 5; ++i) {
+    matrix[i] = Container<int>(0, 5);
+    for (unsigned int j = 0; j < 5; ++j)
+      matrix[i][j] = 10 * (i+1) + j + 1;
+  }
+  cout << "Printing matrix..." << endl;
+  print_matrix(matrix);
+  cout << "Matrix printed." << endl; 
+  return 0;
+}
