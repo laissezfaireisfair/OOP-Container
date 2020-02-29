4070119108f3a08a743336c19d9602b60ecd3525
Andrey Golubev <laissezfaireisfair@protonmail.com>
2020-02-29 15:38:20 +0700

    Format header

--- include/Container.hpp
+++ include/Container.hpp
@@ -17,11 +17,13 @@ namespace Bicycle{
       capacity = length + 1;
       body = sPtr<T[]>(new T[capacity], std::default_delete<T[]>());
     }
+
     Container(usInt const capacityReq) {
       length = 0;
       capacity = capacityReq;
       body = sPtr<T[]>(new T[capacity], std::default_delete<T[]>());
     }
+
     Container(Container const & other) {
       length = other.length;
       capacity = length + 1;
@@ -29,6 +31,7 @@ namespace Bicycle{
       for (usInt i = 0; i < length; ++i)
         body[i] = other.body[i];
     }
+
     Container(T const & value, std::uint64_t const size) {
       length = size;
       capacity = length + 1;
@@ -36,31 +39,38 @@ namespace Bicycle{
       for (usInt i = 0; i < length; ++i)
         body[i] = value;
     }
+
     T &get_back() const {
       if (length == 0)
         throw std::length_error("Zero length container dont have back elem");
       return body[length - 1];
     }
+
     T &get_front() const {
       if (length == 0)
         throw std::length_error("Zero length container dont have front elem");
       return body[0];
     }
+
     T &operator[](std::uint64_t const i) const {
       if (i >= length)
         throw std::out_of_range("Container size is less than adress");
       T & ref = body[i];
       return ref;
     }
+
     std::uint64_t get_length() const {
       return length;
     }
+
     std::uint64_t get_capacity() const {
       return capacity;
     }
+
     bool is_empty() const {
       return length == 0;
     }
+
     void push_back(T const & elem) {
       if (capacity > length) {
         body[length] = elem;
@@ -75,16 +85,19 @@ namespace Bicycle{
       length++;
       body = newBody;
     }
+
     void pop_back() {
       if (length == 0)
         throw std::length_error("Zero length container dont have back elem");
       body[length - 1].~T();
       --length;
     }
+
     void clean() {
       while(!is_empty())
         pop_back();
     }
+
   private:
     usInt length;
     usInt capacity;
